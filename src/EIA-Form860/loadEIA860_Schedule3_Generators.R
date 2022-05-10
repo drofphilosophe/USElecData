889a073c706cd2617889ece536c361627e0686e5
James Archsmith <36709109+drofphilosophe@users.noreply.github.com>
2022-05-10 15:56:22 -0400

    Initial Commit of EIA 860 Schedule 3 (Generators)
    Code to load and clean the generators file (EIA 860 Schedule 3). Currently only processes 2009 and later. Column specs included in the associated JSON file.

--- src/EIA-Form860/loadEIA860_Schedule3_Generators.R
+++ src/EIA-Form860/loadEIA860_Schedule3_Generators.R
@@ -7,10 +7,11 @@ library(readxl)
 library(haven)
 library(here)
 library(yaml)
+library(jsonlite)
 
 #Identifies the project root path using the
 #relative location of this script
-i_am("src/EPA-CEMS/loadEIA860_Schedule6_BoilerStack.R")
+i_am("src/EIA-Form860/loadEIA860_Schedule6_BoilerStack.R")
 
 #Read the project configuration
 read_yaml(here("config.yaml")) -> project.config
@@ -29,6 +30,18 @@ if(is.null(project.config$sources$`EIA-Form860`$`end-year`)) {
   year.end <- as.integer(project.config$sources$`EIA-Form860`$`end-year`)
 }
 
+############################
+## Load a JSON file with column name remapping information
+## Each top-level entry in this list is a conformed column name
+## associated with a list with many candidate original names
+## We'll use this information to conform the myriad column names
+## that show up in the original EIA data
+###########################
+read_json(
+  here(
+    file.path("src","EIA-Form860","EIA860_Schedule3_Generators_Colspec.json")
+    )
+  ) -> colspec
 
 ##########################
 ## Create Output Folders
@@ -105,7 +118,7 @@ detect_start_end <- function(path,sheet) {
 
 
 
-
+all.generators <- tibble()
 for(yr in year.start:year.end) {
   #Does this folder exist?
   if(!dir.exists(file.path(path.EIA860.source,yr))) {
@@ -161,13 +174,161 @@ for(yr in year.start:year.end) {
   ##########################
   ## Process Generator Data in  schedule 3 - You'll need to loop through each sheet
   ##########################
-  writeLines(str_c("I would try to load ", filelist))
+  xlfile = file.path(path.EIA860.source,yr,filelist)
+  sheetlist = excel_sheets(xlfile)
+  for(sheet in sheetlist) {
+    row.bounds = detect_start_end(xlfile,sheet)
+    read_excel(
+      xlfile,
+      sheet=sheet,
+      range=cell_rows(c(row.bounds[1], row.bounds[2]-1)),
+      col_types = "text"
+    ) -> sheet.data
+    
+    orig.names <- names(sheet.data)
+    new.names <- names(colspec)
+    for(o in orig.names) {
+      for(n in new.names) {
+        n.list = unlist(colspec[[n]])
+        if(o %in% n.list) {
+          #Rename the old name to the new name
+          sheet.data %>% rename(!!n := rlang::sym(o)) -> sheet.data
+          #Pop the new name from new.names
+          new.names = new.names[new.names != n]
+          #Pop the old name from orig.names
+          orig.names = orig.names[orig.names != o]
+        }
+      }
+    }
+    
+    writeLines("\n\n")
+    writeLines("------------------------------------------------------------")
+    writeLines(crayon::bold(str_c("Year: ", yr, " Sheet: ", sheet)))
+    writeLines("------------------------------------------------------------")
+    writeLines("The following conformed column names were not found in this file.")
+    writeLines("This is typical as the data content changes over time.")
+    for(n in new.names) {
+      writeLines(n)
+    }
+    
+    writeLines("")
+    writeLines("The following columns in the data file did not have a conformed name")
+    writeLines("They will be dropped.")
+    for(n in orig.names) {
+      writeLines(n)
+    }
+    
+    case_when(
+      sheet %in% c("Exist") ~ "Operating",
+      str_detect(sheet,"[Oo]perable") ~ "Operating",
+      str_detect(sheet,"[Oo]perating") ~ "Operating",
+      sheet %in% c("Ret_IP") ~ "Retired",
+      str_detect(sheet,"[Rr]etired") ~ "Retired",
+      sheet %in% c("Prop","proposed") ~ "Proposed",
+      str_detect(sheet,"[Pp]roposed") ~ "Proposed",
+      TRUE ~ as.character(NA)
+      ) -> sheet.desc
+    
+    if(is.na(sheet.desc)) stop(str_c("UNKNOWN SHEET TYPE: ",sheet))
+    
+    sheet.data %>%
+      select(-any_of(orig.names)) %>%
+      mutate(
+        Year = yr,
+      ) %>%
+      bind_rows(all.generators) -> all.generators
+  }
 }
 
+##########################
+## Here you will clean up the data types
+## Everything has been read in as text to avoid
+## inconsistent guessing of data types
+#########################
+numeric.cols = c(
+  "utility.id",
+  "orispl.code",
+  "nameplate.capacity.mw",
+  "summer.capacity.mw",
+  "winter.capacity.mw",
+  "minimum.load.mw",
+  "nameplate.power.factor",
+  "sector.type.code",
+  "planned.uprate.summer",
+  "planned.uprate.winter",
+  "planned.derate.summer",
+  "planned.derate.winter",
+  "planned.new.nameplate.capacity.mw"
+)
+for(c in numeric.cols) {
+  #Display values that won't recast to a numeric
+  all.generators %>%
+    mutate(
+      temp.xxx = !!rlang::sym(c)
+    ) %>%
+    drop_na(temp.xxx) %>%
+    filter(is.na(as.numeric(temp.xxx))) %>%
+    filter( !( str_trim(temp.xxx) %in% c("","."))) %>%
+    distinct(temp.xxx) -> bad.vals
+  
+  if(nrow(bad.vals) > 0) {
+    writeLines(str_c("Column ",crayon::bold(c)," should be numeric but contains the following non-numeric values:"))
+    print(bad.vals,n=Inf)
+  }
+  
+  all.generators %>%
+    mutate(!!c := as.numeric(!!rlang::sym(c))) -> all.generators
+}
 
-
-
-
+##Convert year/month values to dates
+year.month.cols = c(
+  "first.operating",
+  "planned.uprate.start",
+  "planned.derate.start",
+  "planned.repower",
+  "planned.retirement",
+  "planned.other.mods.start"
+)
+for(ym in year.month.cols) {
+  all.generators %>%
+    rename(
+      .year = !!rlang::sym(str_c(ym,".year")),
+      .month = !!rlang::sym(str_c(ym,".month"))
+    ) %>%
+    mutate(
+      #Replace empty years with NAs, This will short-circuit date conversions
+      .year = if_else(.year %in% c("",".","0"),as.character(NA),.year),
+      #Replace empty/zero/too big months with 7. The assumption is we'll have a valid year
+      #And those cases will convert to July 1st
+      .month = if_else(
+        .month %in% c("",".","0") | suppressWarnings(as.numeric(.month)) > 12,
+        "07",
+        .month
+        ),
+      #Combine year and month columns to form a date on the first of the corresponding month
+      .date = ymd(str_c(
+        .year,
+        .month,
+        "01",
+        sep="-"
+        ))
+    ) -> all.generators
+  
+  #Identify dates that didn't parse so we can augment the code to handle them
+  all.generators %>% 
+    filter(is.na(.date) & !is.na(.month) & !is.na(.year)) %>%
+    select(starts_with(".")) -> no.parse
+  
+  if(nrow(no.parse)>0) {
+    writeLines(str_c("Could not parse dates in column ",c," with the following values"))
+    print(no.parse,n=Inf)
+  }
+  
+  #Rename/remove temporary and redudant variables
+  all.generators %>%
+    rename(!!rlang::sym(str_c(ym,".month")) := .date) %>%
+    select(-any_of(c(".year",".month"))) -> all.generators
+}
 
 
 
