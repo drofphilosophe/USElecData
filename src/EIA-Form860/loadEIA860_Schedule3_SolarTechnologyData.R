37c6ab322654aed0eb7231291286499fe9f193f6
James Archsmith <36709109+drofphilosophe@users.noreply.github.com>
2023-02-06 15:54:09 -0500

    Additional Updates
    Add code for processing EIA-860 Solar Technology Data

---
+++ src/EIA-Form860/loadEIA860_Schedule3_SolarTechnologyData.R
@@ -0,0 +1,429 @@
+################################################
+## Load generator info from EIA Form 860 Schedule 3 - Solar Technology Data
+################################################
+library(tidyverse)
+library(lubridate)
+library(readxl)
+library(haven)
+library(here)
+library(yaml)
+library(jsonlite)
+
+#Identifies the project root path using the
+#relative location of this script
+i_am("src/EIA-Form860/loadEIA860_Schedule3_SolarTechnologyData.R")
+
+#Read the project configuration
+read_yaml(here("config.yaml")) -> project.config
+read_yaml(here("config_local.yaml")) -> project.local.config
+
+path.project <- file.path(project.local.config$output$path)
+version.date <- project.config$`version-info`$`version-date`
+
+path.EIA860.source = file.path(path.project,"data","source","EIA-Form860")
+path.EIA860.out = file.path(path.project,"data","out","EIA-Form860")
+
+#Form 860 Started collecting solar data in 2013
+year.start = max(as.integer(project.config$sources$`EIA-Form860`$`start-year`),2013)
+
+if(is.null(project.config$sources$`EIA-Form860`$`end-year`)) {
+  year.end <- year(today())
+} else {
+  year.end <- as.integer(project.config$sources$`EIA-Form860`$`end-year`)
+}
+
+############################
+## Load a JSON file with column name remapping information
+## Each top-level entry in this list is a conformed column name
+## associated with a list with many candidate original names
+## We'll use this information to conform the myriad column names
+## that show up in the original EIA data
+###########################
+read_json(
+  here(
+    file.path("src","EIA-Form860","EIA860_Schedule3_Solar_Colspec.json")
+    )
+  ) -> colspec
+
+##########################
+## Create Output Folders
+##########################
+dir.create(path.EIA860.out, recursive = TRUE, showWarnings = FALSE)
+
+
+#Detect the starting and ending row of data in the schedule 6 sheets
+#This follows the same approach in every worksheet.
+#Load all of column A. The header rows have some variant of "Utility ID"
+#The table ends at the last row or in the row before we encounter 
+#a value beginning with "NOTE". Return these bounds as a vector.
+detect_start_end <- function(path,sheet) {
+  #Determine the starting and ending row of data
+  #By reading the first column as text
+  read_excel(
+    path,
+    sheet=sheet,
+    range=cell_limits(c(1, 1), c(NA, 1)),
+    col_types=c("text"),
+    col_names="A"
+  ) %>%
+    mutate(n=row_number()) -> one.col
+  
+  ##The first row of data is the column names
+  ##It should be some variant of "Utility ID"
+  one.col %>%
+    filter(str_detect(str_to_upper(A),"UTILITY")) %>%
+    summarize(n=min(n)) %>%
+    pull(n) -> first.row
+  
+  #Abort if we can't detect row headers
+  if(first.row == Inf) 
+    stop(
+      str_c(
+        "Unable to detect row headers in ",
+        "\nfile: ", path,
+        "\nsheet: ", sheet
+      )
+    )
+  
+  #There is frequently a "note" column at the end.
+  #Read column 1 only and look for the note column 
+  #This generates a warning if there is no note. 
+  #We'll suppress that
+  suppressWarnings(
+    one.col %>%
+      filter(str_detect(A,"NOTE")) %>%
+      summarize(n=min(n)) %>%
+      pull(n) -> last.row
+  )
+  
+  
+  #print(last.row)
+  #the max() function above will return Inf, but we
+  #Want an NA if NOTE does not exist
+  if(last.row==Inf) last.row = as.integer(NA)
+  
+  return(c(first.row,last.row))
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+all.solar <- tibble()
+for(yr in year.start:year.end) {
+  #Does this folder exist?
+  if(!dir.exists(file.path(path.EIA860.source,yr))) {
+    warning(str_c("Source data folder for ",yr," does not exist. Skipping"))
+    next
+  }
+  
+  #List files in this folder
+  filelist <- list.files(file.path(path.EIA860.source,yr))
+  
+  if(length(filelist) == 0) {
+    warning(str_c("No files found in source data folder for ",yr,". Skipping."))
+    next
+  }
+  
+  
+  ##########################
+  ##########################
+  ## Identify the Schedule 3 Solar file
+  ##########################
+  ##########################
+  
+  ######################
+  ## This block implements logic to identify the Form 6 file
+  ## The file names vary by year, so there will be code blocks
+  ## for sets of years where the filenames follow similar patterns
+  ######################
+  if(yr >= 2013) {
+    ## 2013 and later - look for files with "Solar" in their name
+    filelist <- filelist[str_detect(filelist,"Solar")]
+    ##Remove any files with tildes in their names. These are temporary excel files
+    filelist <- filelist[!str_detect(filelist,"\\~")]
+  } else {
+    #Catch all years not handled by this block
+    warning(str_c("We're not currently equipped to handle files from ", yr,". Skipping"))
+    next
+  }
+  
+  ##Check that we have only exactly one file in the filelist
+  ##We should always be able to identify exactly one file.
+  ##Otherwise, throw an error
+  if(length(filelist) == 0) {
+    stop(str_c("Files exist in source data folder for ",yr," but none match a Form 3 Solar data file name."))
+    next
+  }
+  
+  if(length(filelist) > 1) {
+    stop(str_c("Multiple files in source data folder for ",yr," match a Form 3 Solar data file name."))
+    next
+  }
+  
+  
+  ##########################
+  ## Process Generator Data in  schedule 3 - You'll need to loop through each sheet
+  ##########################
+  xlfile = file.path(path.EIA860.source,yr,filelist)
+  sheetlist = excel_sheets(xlfile)
+  for(sheet in sheetlist) {
+    row.bounds = detect_start_end(xlfile,sheet)
+    read_excel(
+      xlfile,
+      sheet=sheet,
+      range=cell_rows(c(row.bounds[1], row.bounds[2]-1)),
+      col_types = "text"
+    ) -> sheet.data
+    
+    orig.names <- names(sheet.data)
+    new.names <- names(colspec)
+    for(o in orig.names) {
+      for(n in new.names) {
+        n.list = unlist(colspec[[n]])
+        if(o %in% n.list) {
+          #Rename the old name to the new name
+          sheet.data %>% rename(!!n := rlang::sym(o)) -> sheet.data
+          #Pop the new name from new.names
+          new.names = new.names[new.names != n]
+          #Pop the old name from orig.names
+          orig.names = orig.names[orig.names != o]
+        }
+      }
+    }
+    
+    writeLines("\n\n")
+    writeLines("------------------------------------------------------------")
+    writeLines(crayon::bold(str_c("Year: ", yr, " Sheet: ", sheet)))
+    writeLines("------------------------------------------------------------")
+    writeLines("The following conformed column names were not found in this file.")
+    writeLines("This is typical as the data content changes over time.")
+    for(n in new.names) {
+      writeLines(n)
+    }
+    
+    writeLines("")
+    writeLines("The following columns in the data file did not have a conformed name")
+    writeLines("They will be dropped.")
+    for(n in orig.names) {
+      writeLines(n)
+    }
+    
+    case_when(
+      sheet %in% c("Operable") ~ "Operating",
+      sheet %in% c("Retired and Canceled") ~ "Retired",
+      #str_detect(sheet,"[Pp]roposed") ~ "Proposed",
+      TRUE ~ as.character(NA)
+      ) -> sheet.desc
+    
+    if(is.na(sheet.desc)) stop(str_c("UNKNOWN SHEET TYPE: ",sheet))
+    
+    sheet.data %>%
+      select(-any_of(orig.names)) %>%
+      mutate(
+        year = yr,
+      ) %>%
+      bind_rows(all.solar) -> all.solar
+  }
+}
+
+
+
+
+##########################
+## Here you will clean up the data types
+## Everything has been read in as text to avoid
+## inconsistent guessing of data types
+#########################
+numeric.cols = c(
+  "utility.id",
+  "orispl.code",
+  "nameplate.capacity.mw",
+  "summer.capacity.mw",
+  "winter.capacity.mw",
+  "sector.type.code",
+  "azimuth.angle",
+  "tilt.angle",
+  "net.capacity.dc.mw",
+  "net.capacity.under.stc",
+  "virtual.net.metering.capacity.dc.mw"
+)
+for(c in numeric.cols) {
+  #Display values that won't recast to a numeric
+  all.solar %>%
+    mutate(
+      temp.xxx = !!rlang::sym(c)
+    ) %>%
+    drop_na(temp.xxx) %>%
+    filter(is.na(as.numeric(temp.xxx))) %>%
+    filter( !( str_trim(temp.xxx) %in% c("","."))) %>%
+    distinct(temp.xxx) -> bad.vals
+  
+  if(nrow(bad.vals) > 0) {
+    writeLines(str_c("Column ",crayon::bold(c)," should be numeric but contains the following non-numeric values:"))
+    print(bad.vals,n=Inf)
+  }
+  
+  all.solar %>%
+    mutate(!!c := as.numeric(!!rlang::sym(c))) -> all.solar
+}
+
+boolean.cols = c(
+  "has.lense.mirror",
+  "has.single.axis.tracking",
+  "has.dual.axis.tracking",
+  "has.fixed.tilt",
+  "has.fixed.tilt.eastwest",
+  "has.parabolic.trough",
+  "has.linear.fresnel",
+  "has.power.tower",
+  "has.dish.engine",
+  "has.other.solar.tech",
+  "is.crystalline.silicon",
+  "is.thinfilm.CdTe",
+  "is.thinfilm.ASi",
+  "is.thinfilm.CIGS",
+  "is.thinfilm.other",
+  "is.other.material",
+  "has.net.metering",
+  "has.virtual.net.metering"
+)
+for(c in boolean.cols) {
+  all.solar %>%
+    mutate(
+      temp.xxx = !!rlang::sym(c),
+      temp.xxx = case_when(
+        str_to_upper(temp.xxx) %in% c("Y","YES","TRUE","T","1") ~ "TRUE",
+        str_to_upper(temp.xxx) %in% c("N","NO","FALSE","F","0") ~ "FALSE",
+        str_trim(str_to_upper(temp.xxx)) %in% c("","NA") ~ "NA",
+        TRUE ~ temp.xxx
+      )
+    ) -> all.solar
+  
+  #Display values that won't recast to a boolean
+  all.solar %>%
+    filter(!(temp.xxx %in% c("TRUE","FALSE","NA"))) %>%
+    drop_na(temp.xxx) %>%
+    distinct(temp.xxx) -> bad.vals
+  
+  if(nrow(bad.vals) > 0) {
+    writeLines(str_c("Column ",crayon::bold(c)," should be boolean but contains the following non-numeric values:"))
+    print(bad.vals,n=Inf)
+  }
+  
+  all.solar %>%
+    mutate(
+      "{c}" := case_when(
+        temp.xxx == "TRUE" ~ TRUE,
+        temp.xxx == "FALSE" ~ FALSE,
+        temp.xxx == "NA" ~ as.logical(NA),
+        TRUE ~ as.logical(NA)
+      )
+    ) -> all.solar
+}
+
+
+
+
+
+
+
+
+##Convert year/month values to dates
+year.month.cols = c(
+  "first.operating",
+  "retired"
+)
+for(ym in year.month.cols) {
+  all.solar %>%
+    rename(
+      .year = !!rlang::sym(str_c(ym,".year")),
+      .month = !!rlang::sym(str_c(ym,".month"))
+    ) %>%
+    mutate(
+      #Replace empty years with NAs, This will short-circuit date conversions
+      .year = if_else(.year %in% c("",".","0"),as.character(NA),.year),
+      #Replace empty/zero/too big months with 7. The assumption is we'll have a valid year
+      #And those cases will convert to July 1st
+      .month = if_else(
+        .month %in% c("",".","0") | suppressWarnings(as.numeric(.month)) > 12,
+        "07",
+        .month
+        ),
+      #Combine year and month columns to form a date on the first of the corresponding month
+      .date = ymd(str_c(
+        .year,
+        .month,
+        "01",
+        sep="-"
+        ))
+    ) -> all.solar
+  
+  #Identify dates that didn't parse so we can augment the code to handle them
+  all.solar %>% 
+    filter(is.na(.date) & !is.na(.month) & !is.na(.year)) %>%
+    select(starts_with(".")) -> no.parse
+  
+  if(nrow(no.parse)>0) {
+    writeLines(str_c("Could not parse dates in column ",c," with the following values"))
+    print(no.parse,n=Inf)
+  }
+  
+  #Rename/remove temporary and redudant variables
+  all.solar %>%
+    rename(!!rlang::sym(str_c(ym,".month")) := .date) %>%
+    select(-any_of(c(".year",".month"))) -> all.solar
+}
+
+
+
+
+####################################################
+####################################################
+####################################################
+## Write Output Files
+####################################################
+####################################################
+####################################################
+
+
+
+###############
+
+###############
+if("rds" %in% project.local.config$output$formats) {
+  dir.create(file.path(path.EIA860.out,"rds"),showWarnings = FALSE)
+
+  ## Boiler-Stack
+  all.solar %>%
+    write_rds(
+      file.path(path.EIA860.out,"rds","Form860_Schedule3_Solar.rds.gz"),
+      compress="gz"
+    )
+
+}
+
+if("dta" %in% project.local.config$output$formats) {
+  dir.create(file.path(path.EIA860.out,"stata"),showWarnings = FALSE)
+
+  ## Boiler-Stack
+  all.solar %>%
+    rename_all(.funs=list( ~ str_replace_all(.,"[\\.\\s]", "_"))) %>%
+    rename_all(.funs=list(~str_sub(.,1,31))) %>%
+    write_dta(file.path(path.EIA860.out,"stata","Form860_Schedule3_Solar.dta"))
+
+}
+
+
--- src/NREL-PVWatts/get-PVWatts.py
+++ src/NREL-PVWatts/get-PVWatts.py
@@ -8,11 +8,13 @@
 ## and only download data files that have changed.
 #######################
 import urllib.request
+import urllib.parse
 import os
 import datetime as dt
 import io
 import yaml
 import csv
+import json
 
 #########################
 ## Load and register the configuration
@@ -54,19 +56,51 @@ API_key = projectLocalConfig["auth"]["NREL-API-key"]
 #######################
 class PVWatts :
 
+    MODULE_TYPES = {
+        'Standard' : 0,
+        'Premium' : 1,
+        'Thin Film' : 2
+        }
+
+    ARRAY_TYPES = {
+        'Fixed - Open Rack' : 0,
+        'Fixed - Roof Mounted' : 1,
+        '1-Axis' : 2,
+        '1-Axis Backtracking' : 3,
+        '2-Axis' : 4
+    }
+
+    DATASET_NAMES = ['nsrdb','tmy2','tmy3','intl']
+
+    TIMEFRAMES = ['monthly','hourly']
+    
     def __init__(self,url=None,api_key=None,api_version=8) :
 
         #This class only supports JSON responses
         self.response_format = "json"
 
+        #Request headers in a dictionary. We include an
+        #empty reference to the API key by default
+        self.request_headers = {
+            'X-Api-Key' : ''
+            }
+        
         #Init class parameters
-        self.set_url(url)
-        self.set_api_key(api_key)
+        if url is not None :
+            self.set_url(url)
+        if api_key is not None :
+            self.set_api_key(api_key)
+            
         self.set_api_version(api_version)
 
         #Init a dictionary that will hold query parameters in name : value
         #format
         self.query_params = {}
+
+        #Initialize other class data
+        self.query_url = None
+        self.request = None
+        self.response_json = None
         
 
     def set_url(self,url) :
@@ -74,6 +108,7 @@ class PVWatts :
 
     def set_api_key(self,k) :
         self.api_key = k
+        self.request_headers['X-Api-Key'] = k
 
     def set_api_version(self,v) :
         self.api_version = int(v)
@@ -92,21 +127,131 @@ class PVWatts :
         elif param in self.query_params :
             self.query_params[param] = float(x)
         else :
-            self.query_params.update( {param : float(x) }
+            self.query_params.update( {param : float(x) } )
 
+    def _set_query_int(self,param,x) :
+        if x is None :
+            if param in self.query_params :
+                del self.query_params[param]
+        elif param in self.query_params :
+            self.query_params[param] = int(x)
+        else :
+            self.query_params.update( {param : int(x) } )
+
+    def _set_query_str(self,param,x) :
+        if x is None :
+            if param in self.query_params :
+                del self.query_params[param]
+        elif param in self.query_params :
+            self.query_params[param] = str(x)
+        else :
+            self.query_params.update( {param : str(x) } )                                
+                                      
     def query_system_capacity(self,x) :
         self._set_query_float('system_capacity',x)
 
     def query_module_type(self,x) :
-        
+        if type(x) is str :
+            x = self.MODULE_TYPES[x]
             
+        self._set_query_int('module_type',x)
+
+    def query_losses(self,x) :
+        self._set_query_float('losses',x)
+
+    def query_array_type(self,x) :
+        if type(x) is str :
+            x = self.ARRAY_TYPES[x]
+
+        self._set_query_int('array_type',x)
+
+    def query_tilt(self,x) :
+        self._set_query_float('tilt',x)
+
+    def query_azimuth(self,x) :
+        self._set_query_float('azimuth',x)
+
+    def query_address(self,x) :
+        raise ValueError("Addresses not implemented")
+
+    def query_latitude(self,x) :
+        self._set_query_float('lat',x)
+
+    def query_longitude(self,x) :
+        self._set_query_float('lon',x)
+
+    def query_dataset(self,x) :
+        if x not in self.DATASET_NAMES :
+            raise ValueError("Unknown dataset name")
+        self._set_query_str('dataset',x)
+
+    def query_radius(self,x) :
+        self._set_query_int('radius',x)
+
+    def query_timeframe(self,x) :
+        if x not in self.TIMEFRAMES :
+            raise ValueError("Unknown timeframe")
+        self._set_query_str('timeframe',x)
             
+    def query_dc_ac_ratio(self,x) :
+        self._set_query_float('dc_ac_ratio',x)
+
+    def query_gcr(self,x) :
+        self._set_query_float('gcr',x)
+
+    def query_inv_eff(self,x) :
+        self._set_query_float('inv_eff',x)
+
+    def query_bifaciality(self,x) :
+        self._set_query_float('bifaciality',x)
+
+    def query_albedo(self,x) :
+        self._set_query_float('albedo',x)
+
+    def query_use_wf_albedo(self,x) :
+        if type(x) is bool :
+            x = int(x)
+        self._set_query_int('use_wf_albedo',x)
+
+    def query_soiling(self,x) :
+        raise ValueError("Soiling not implemented")
+
+
+    ###########################
+    ## Query Methods
+    ###########################
+    #Construct a request object
+    def get_request(self) :
+        #Contruct the URL
+        self.query_url = f"{self.url}/api/pvwatts/v{self.api_version}.{self.response_format}?" + \
+                    urllib.parse.urlencode(self.query_params)
         
+        self.request = urllib.request.Request(self.query_url,headers=self.request_headers)
 
 
+    def run_query(self) :
+        with io.BytesIO() as buf :
+            with urllib.request.urlopen(self.request) as resp :
+                buf.write(resp.read())
+            buf.seek(0)
+            self.response_json = json.load(buf)
+            
 
 
-
+P = PVWatts(url=base_URL,api_key=API_key)
+P.query_timeframe('hourly')
+P.query_array_type('Fixed - Open Rack')
+P.query_module_type('Premium')
+P.query_losses(0)
+P.query_latitude(39.98587)
+P.query_longitude(-76.94105)
+P.query_system_capacity(1000)
+P.query_tilt(20)
+P.query_azimuth(165)
+P.get_request()
+P.run_query()
+print(P.response_json.keys())
+#print(P.query_url + f"&api_key={P.api_key}")
 
 
         
